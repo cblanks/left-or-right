0e666eabb1a918535cff10ba64caa690ac752595
= <=>
2016-04-23 19:22:30 +0100

    added counts, central image and local storage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,40 @@
  * Controller of the leftOrRightApp
  */
 angular.module('leftOrRightApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $timeout, localStorageService) {
+    
+    $scope.currentlyLeft = 0 === Math.round(Math.random());
+    $scope.getCurrentSide = function() {
+      if($scope.currentlyLeft === true) { 
+        return "left";
+      } else {
+        return "right";
+      }
+    };
+    $scope.period = 10000; // milliseconds
+    
+    function setFigCaption() {
+      $scope.currentlyLeft = !$scope.currentlyLeft;
+      $scope.figCaption = "I'm breathing from the " + $scope.getCurrentSide() + ".";
+      $timeout(function() { setFigCaption(); }, $scope.period);
+    }
+    setFigCaption();
+    
+    $scope.data = localStorageService.get('data') || {
+      left: [],
+      right: []
+    };
+
+    $scope.$watch('data', function () {
+      localStorageService.set('data', $scope.data);
+    }, true);
+    
+    $scope.incrementLeft = function() {
+      $scope.data.left.push(new Date());
+    };
+
+    $scope.incrementRight = function() {
+      $scope.data.right.push(new Date());
+    };
+
+});
